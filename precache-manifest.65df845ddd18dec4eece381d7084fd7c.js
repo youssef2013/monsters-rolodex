3549c02010780b6a1638ea93cf30ef9361eaf109
y.benkhallouf-ext <y.benkhallouf-ext@lefebvre-sarrut.eu>
2020-01-03 16:56:08 +0100

    Updates

---
+++ precache-manifest.65df845ddd18dec4eece381d7084fd7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d171dde6cbb4f6135f1b11f0db6fce6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4daf9db12fd190f60b05",
+    "url": "/monsters-rolodex/static/css/main.7e8be894.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4daf9db12fd190f60b05",
+    "url": "/monsters-rolodex/static/js/main.9e757459.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
